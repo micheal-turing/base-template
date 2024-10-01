ce4caabd5590461017614a5644363ca3706f0f84
micheal-turing <micheal.o1@turing.com>
2024-10-01 21:11:30 +0100

    twst

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/components/ui/card';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
+
 const FruitSelector = () => {
   const [selectedFruit, setSelectedFruit] = useState('');
 
